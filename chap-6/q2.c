43e7b663d12b520b666bf7a1e88c7c458aa2213f
George Castillo <gmcastil@gmail.com>
2024-04-14 07:14:36 -0600

    Finished system call timing using __rdtscp() intrinsic.

--- chap-6/q2.c
+++ chap-6/q2.c
@@ -16,15 +16,108 @@
 #include <x86intrin.h>
 #endif
 
+/* Sample text file to open for reading 0 bytes */
+#define SAMPLE_FILE	"sample.txt"
+
 /* This could be passed in as an argument, but I'm running in a VM with 2 cores */
 #define NUM_CPUS	2
 /* We wish to constrain our process and its children to the last CPU */
 #define BOUND_CPU	(NUM_CPUS - 1)
+/* How many samples to measure */
+#define NUM_DEFAULT_ITERATIONS		10
+/* How many initial values ot skip when measuring */
+#define NUM_WARMUP	2
 
-int main()
+struct meas_results {
+	unsigned long long	mean;
+	unsigned long long	max;
+	unsigned long long	min;
+	unsigned int		tsc_aux;
+	int			samples;
+};
+
+void print_results(struct meas_results *results)
 {
-	unsigned long long rt_val;
-	unsigned int tsc;
+	if (results == NULL) {
+		errexit("Null pointer check failed");
+	} else {
+		printf("%-15s%lld\n", "Mean:", results->mean);
+		printf("%-15s%lld\n", "Max:", results->max);
+		printf("%-15s%lld\n", "Min:", results->min);
+		printf("%-15s%d\n", "Samples:", results->samples);
+	}
+}
+
+void meas_syscall_gettime(const int num_iterations, const int num_warmup)
+{
+
+	int fd;
+	fd = open(SAMPLE_FILE, O_CREAT | O_RDONLY, S_IRWXU);
+}
+
+void meas_syscall_intrin(const int num_iterations, const int num_warmup)
+{
+
+	struct meas_results results;
+
+	int fd;
+
+	unsigned long long before, after;
+	unsigned long long delta;
+
+	/* The IA32_TSC_AUX MSR (signature value) is stored here */	
+	unsigned int tsc_aux;
+
+	fd = open(SAMPLE_FILE, O_CREAT | O_RDONLY, S_IRWXU);
+	if (fd == -1) {
+		errexit(strerror(errno));
+	}
+
+	results.min = 0;
+	results.max = 0;
+	results.samples = 0;
+	/* Use this member as an accumulator and then divide at the end */
+	results.mean = 0;
+
+	for (int i=0; i<num_iterations; i++) {
+
+		/* Measure our system call */
+		before = __rdtscp(&tsc_aux);
+		read(fd, NULL, 0);
+		after = __rdtscp(&tsc_aux);
+
+		/* Ignore the first few measurements to let the cache warmup */
+		if (i >= num_warmup) {
+			delta = (after - before);
+			results.mean += delta;
+			if (delta > results.max) {
+				results.max = delta;
+			}
+			if ( (delta < results.min ) && ( delta != 0 ) ) {
+				results.min = delta;
+			}
+			results.samples++;
+#ifdef RDTSCP_DEBUG
+			fprintf(stderr, "Proc ID: %u\n", tsc_aux);
+#endif  /* RDTSCP_DEBUG */
+		}
+	}
+	results.mean = results.mean / results.samples;
+
+	print_results(&results);
+	
+	close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+	int num_iterations;
+
+	if (argc == 1) {
+		num_iterations = NUM_DEFAULT_ITERATIONS;
+	} else {
+		num_iterations = atoi(argv[1]);
+	}
 
 	cpu_set_t mask;
 
@@ -43,14 +136,17 @@ int main()
 
 	CPU_ZERO(&mask);
 	CPU_SET(BOUND_CPU, &mask);
-	if (!CPU_ISSET(BOUND_CPU, &mask)) {
-		errexit("Could not set CPU %d in mask", BOUND_CPU);
-	}
+
 	/* Note that this just sets the bits in the mask, it doesn't actually
 	 * apply them yet (which is why setting the BOUND_CPU to one that isn't
 	 * in the system doesn't generate an error).  Setting a bit in a mask
 	 * doesn't require the hardware to do anything, it's just a bit.
 	 */
+	if (!CPU_ISSET(BOUND_CPU, &mask)) {
+		errexit("Could not set CPU %d in mask", BOUND_CPU);
+	}
+
+	/* The actual change in processor affinity occurs here */
 	if (sched_setaffinity(0, sizeof(mask), &mask) == -1) {
 		if (errno == EINVAL) {
 			errexit("Bit mask contains no processors currently on the system");
@@ -58,11 +154,12 @@ int main()
 			errexit(strerror(errno));
 		}
 	}
+	printf("Processor afinity bound to CPU ID %d\n", BOUND_CPU);
 
-	rt_val = __rdtscp(&tsc);
-
-	printf("RDTSCP register: 0x%llx\n", rt_val);
-	printf("Proc ID: %u\n", tsc);
+	/* Measure using x86_64 intrinsic */
+	meas_syscall_intrin(num_iterations, NUM_WARMUP);
+	/* Measure using a less accurate metric */
+	meas_syscall_gettime(num_iterations, NUM_WARMUP);
 
 	return 0;
 }
