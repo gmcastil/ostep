57a194724b952a494ff3a5ba24fb9b5ea438946d
George Castillo <gcastillo@vizrt.com>
2024-04-18 09:10:07 -0600

    Adding the prompt portion of the shell as an additional source file

--- proj/gsh/shell.c
+++ proj/gsh/shell.c
@@ -1,5 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include "prompt.h"
+
 int main()
 {
+
+	char *line;
+
+	/* Main loop of the shell */
+	while (1) {
+
+		line = readline(gsh_get_prompt());
+		if (line == NULL) {
+			free(line);
+			gsh_prompt_cleanup();
+			return 0;
+		}
+		if (line != NULL) {
+			free(line);
+		}
+	}
+
 	return 0;
 }
 
